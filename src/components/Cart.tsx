aaba80e810fb90400a80a157473e71306358ef4c
hamzatalha77 <hamza_ta7@hotmail.com>
2024-02-28 19:58:49 +0100

    commit

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,53 +1,9 @@
-import React from 'react'
-
 const Cart = () => {
   return (
-    <div>
+    <div className="fixed bg-slate-200 z-50 w-[420px] shadow">
       <i className="bx bx-x"></i>
       <h2>My Cart</h2>
       <div>
-        <article>
-          <div>
-            <img src="" alt="" />
-          </div>
-          <div>
-            <h3>Windbreaker</h3>
-            <span>12$</span>
-            <div>
-              <div>
-                <span>
-                  <i className="bx bx-minus"></i>
-                </span>
-                <span>1</span>
-                <span>
-                  <i className="bx bx-plus"></i>
-                </span>
-              </div>
-              <i className="bx bx-trash-alt"></i>
-            </div>
-          </div>
-        </article>
-        <article>
-          <div>
-            <img src="" alt="" />
-          </div>
-          <div>
-            <h3>Windbreaker</h3>
-            <span>12$</span>
-            <div>
-              <div>
-                <span>
-                  <i className="bx bx-minus"></i>
-                </span>
-                <span>1</span>
-                <span>
-                  <i className="bx bx-plus"></i>
-                </span>
-              </div>
-              <i className="bx bx-trash-alt"></i>
-            </div>
-          </div>
-        </article>
         <article>
           <div>
             <img src="" alt="" />
