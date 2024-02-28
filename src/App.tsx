7d5969242073f0655e37213dc6d01a9fda31a0df
hamzatalha77 <hamza_ta7@hotmail.com>
2024-02-28 12:49:19 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Header from './components/Header'
+import home from './layouts/home'
 const App = () => {
-  return <div></div>
+  return (
+    <Router>
+      <Header />
+      <Route path="/" Component={home} />
+    </Router>
+  )
 }
 
 export default App
