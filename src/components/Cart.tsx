f387a6125a889eff9436bacbe408d102a24c6084
hamzatalha77 <hamza_ta7@hotmail.com>
2024-02-28 19:46:04 +0100

    commit

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,7 +1,77 @@
 import React from 'react'
 
 const Cart = () => {
-  return <div></div>
+  return (
+    <div>
+      <i className="bx bx-x"></i>
+      <h2>My Cart</h2>
+      <div>
+        <article>
+          <div>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <h3>Windbreaker</h3>
+            <span>12$</span>
+            <div>
+              <div>
+                <span>
+                  <i className="bx bx-minus"></i>
+                </span>
+                <span>1</span>
+                <span>
+                  <i className="bx bx-plus"></i>
+                </span>
+              </div>
+              <i className="bx bx-trash-alt"></i>
+            </div>
+          </div>
+        </article>
+        <article>
+          <div>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <h3>Windbreaker</h3>
+            <span>12$</span>
+            <div>
+              <div>
+                <span>
+                  <i className="bx bx-minus"></i>
+                </span>
+                <span>1</span>
+                <span>
+                  <i className="bx bx-plus"></i>
+                </span>
+              </div>
+              <i className="bx bx-trash-alt"></i>
+            </div>
+          </div>
+        </article>
+        <article>
+          <div>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <h3>Windbreaker</h3>
+            <span>12$</span>
+            <div>
+              <div>
+                <span>
+                  <i className="bx bx-minus"></i>
+                </span>
+                <span>1</span>
+                <span>
+                  <i className="bx bx-plus"></i>
+                </span>
+              </div>
+              <i className="bx bx-trash-alt"></i>
+            </div>
+          </div>
+        </article>
+      </div>
+    </div>
+  )
 }
 
 export default Cart
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import 'boxicons'
 
 const Header = () => {
   return (
