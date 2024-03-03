4ec4c3736c6165c227be9e7161b676fd902d2da1
hamzatalha77 <hamza_ta7@hotmail.com>
2024-03-03 18:43:12 +0100

    commit

--- src/components/Home/NewArrivals.tsx
+++ src/components/Home/NewArrivals.tsx
@@ -5,7 +5,9 @@ import 'swiper/css'
 const NewArrivals = () => {
   return (
     <section className="section">
-      <h2 className="text-4xl text-center mb-12">New Arrivals</h2>
+      <h2 className="text-4xl text-center mb-12 section__title">
+        New Arrivals
+      </h2>
       <div className="container pt-4">
         <Swiper
           slidesPerView={'auto'}
@@ -40,17 +42,20 @@ const NewArrivals = () => {
               <i className="bx bx-cart-alt text-xl"></i>
             </a>
           </SwiperSlide>
-          <SwiperSlide className="relative bg-white w-[310px] px-[0] py-8 rounded-2xl text-center group overflow-hidden self-center">
+
+          <SwiperSlide className="relative bg-white w-[310px] px-[0] py-8 rounded-2xl text-center group overflow-hidden self-center new__content">
             <div className="absolute top-[8%] left-[8%] bg-red-600 text-white text-sm px-[.5rem] py-[.25rem] rounded">
               New
             </div>
             <img
               src="/img/new-1.png"
               alt=""
-              className="w-[150px] inline mb-4 group-hover:-translate-y-[.5rem] [transition:.3s]"
+              className="w-[150px] inline mb-4 group-hover:-translate-y-[.5rem] [transition:.3s] new__img"
             />
-            <h3 className="text-xl font-medium">WindBreaker Jacket</h3>
-            <span className="block text-sm mb-4">Accessory</span>
+            <h3 className="text-xl font-medium new__title">
+              WindBreaker Jacket
+            </h3>
+            <span className="block text-sm mb-4 new__subtitle">Accessory</span>
             <div className="inline-flex items-center gap-x-2">
               <span className="font-medium text-gray-900">$14.99</span>
               <span className="text-red-600 text-sm line-through font-medium">
@@ -64,17 +69,20 @@ const NewArrivals = () => {
               <i className="bx bx-cart-alt text-xl"></i>
             </a>
           </SwiperSlide>
-          <SwiperSlide className="relative bg-white w-[310px] px-[0] py-8 rounded-2xl text-center group overflow-hidden self-center">
+
+          <SwiperSlide className="relative bg-white w-[310px] px-[0] py-8 rounded-2xl text-center group overflow-hidden self-center new__content">
             <div className="absolute top-[8%] left-[8%] bg-red-600 text-white text-sm px-[.5rem] py-[.25rem] rounded">
               New
             </div>
             <img
               src="/img/new-1.png"
               alt=""
-              className="w-[150px] inline mb-4 group-hover:-translate-y-[.5rem] [transition:.3s]"
+              className="w-[150px] inline mb-4 group-hover:-translate-y-[.5rem] [transition:.3s] new__img"
             />
-            <h3 className="text-xl font-medium">WindBreaker Jacket</h3>
-            <span className="block text-sm mb-4">Accessory</span>
+            <h3 className="text-xl font-medium new__title">
+              WindBreaker Jacket
+            </h3>
+            <span className="block text-sm mb-4 new__subtitle">Accessory</span>
             <div className="inline-flex items-center gap-x-2">
               <span className="font-medium text-gray-900">$14.99</span>
               <span className="text-red-600 text-sm line-through font-medium">
@@ -88,17 +96,20 @@ const NewArrivals = () => {
               <i className="bx bx-cart-alt text-xl"></i>
             </a>
           </SwiperSlide>
-          <SwiperSlide className="relative bg-white w-[310px] px-[0] py-8 rounded-2xl text-center group overflow-hidden self-center">
+
+          <SwiperSlide className="relative bg-white w-[310px] px-[0] py-8 rounded-2xl text-center group overflow-hidden self-center new__content">
             <div className="absolute top-[8%] left-[8%] bg-red-600 text-white text-sm px-[.5rem] py-[.25rem] rounded">
               New
             </div>
             <img
               src="/img/new-1.png"
               alt=""
-              className="w-[150px] inline mb-4 group-hover:-translate-y-[.5rem] [transition:.3s]"
+              className="w-[150px] inline mb-4 group-hover:-translate-y-[.5rem] [transition:.3s] new__img"
             />
-            <h3 className="text-xl font-medium">WindBreaker Jacket</h3>
-            <span className="block text-sm mb-4">Accessory</span>
+            <h3 className="text-xl font-medium new__title">
+              WindBreaker Jacket
+            </h3>
+            <span className="block text-sm mb-4 new__subtitle">Accessory</span>
             <div className="inline-flex items-center gap-x-2">
               <span className="font-medium text-gray-900">$14.99</span>
               <span className="text-red-600 text-sm line-through font-medium">
@@ -112,17 +123,20 @@ const NewArrivals = () => {
               <i className="bx bx-cart-alt text-xl"></i>
             </a>
           </SwiperSlide>
-          <SwiperSlide className="relative bg-white w-[310px] px-[0] py-8 rounded-2xl text-center group overflow-hidden self-center">
+
+          <SwiperSlide className="relative bg-white w-[310px] px-[0] py-8 rounded-2xl text-center group overflow-hidden self-center new__content">
             <div className="absolute top-[8%] left-[8%] bg-red-600 text-white text-sm px-[.5rem] py-[.25rem] rounded">
               New
             </div>
             <img
               src="/img/new-1.png"
               alt=""
-              className="w-[150px] inline mb-4 group-hover:-translate-y-[.5rem] [transition:.3s]"
+              className="w-[150px] inline mb-4 group-hover:-translate-y-[.5rem] [transition:.3s] new__img"
             />
-            <h3 className="text-xl font-medium">WindBreaker Jacket</h3>
-            <span className="block text-sm mb-4">Accessory</span>
+            <h3 className="text-xl font-medium new__title">
+              WindBreaker Jacket
+            </h3>
+            <span className="block text-sm mb-4 new__subtitle">Accessory</span>
             <div className="inline-flex items-center gap-x-2">
               <span className="font-medium text-gray-900">$14.99</span>
               <span className="text-red-600 text-sm line-through font-medium">
@@ -136,17 +150,20 @@ const NewArrivals = () => {
               <i className="bx bx-cart-alt text-xl"></i>
             </a>
           </SwiperSlide>
-          <SwiperSlide className="relative bg-white w-[310px] px-[0] py-8 rounded-2xl text-center group overflow-hidden self-center">
+
+          <SwiperSlide className="relative bg-white w-[310px] px-[0] py-8 rounded-2xl text-center group overflow-hidden self-center new__content">
             <div className="absolute top-[8%] left-[8%] bg-red-600 text-white text-sm px-[.5rem] py-[.25rem] rounded">
               New
             </div>
             <img
               src="/img/new-1.png"
               alt=""
-              className="w-[150px] inline mb-4 group-hover:-translate-y-[.5rem] [transition:.3s]"
+              className="w-[150px] inline mb-4 group-hover:-translate-y-[.5rem] [transition:.3s] new__img"
             />
-            <h3 className="text-xl font-medium">WindBreaker Jacket</h3>
-            <span className="block text-sm mb-4">Accessory</span>
+            <h3 className="text-xl font-medium new__title">
+              WindBreaker Jacket
+            </h3>
+            <span className="block text-sm mb-4 new__subtitle">Accessory</span>
             <div className="inline-flex items-center gap-x-2">
               <span className="font-medium text-gray-900">$14.99</span>
               <span className="text-red-600 text-sm line-through font-medium">
