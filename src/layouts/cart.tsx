497775b664cf614523ed43c03d0c88841c8340e7
hamzatalha77 <hamza_ta7@hotmail.com>
2024-03-26 16:53:09 +0000

    commit

--- src/layouts/cart.tsx
+++ src/layouts/cart.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
 
 const Cart = () => {
-  return <div></div>
+  return (
+    <section className="section container">
+      <h2 className="breadcrumb__title">Shopping Cart Page</h2>
+      <h3 className="breadcrumb__subtitle">
+        Home {'>'} <span className="text-red-600">Shopping Cart</span>
+      </h3>
+      <div>
+        <div className="first-div">
+          <div className="titles">
+            <h2>Shopping Cart</h2>
+            <h2>3 Items</h2>
+          </div>
+          <div></div>
+        </div>
+        <div className="second-div"></div>
+      </div>
+    </section>
+  )
 }
 
 export default Cart
