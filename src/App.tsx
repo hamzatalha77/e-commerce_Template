660a40168b634b304a5e402da8dc2b4d97ac25bc
hamzatalha77 <hamza_ta7@hotmail.com>
2024-04-27 13:14:53 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ const App = () => {
         <Route path="/blog" element={<Blog />} />
         <Route path="/product" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/payment" element={<Payment />} />
+        <Route path="/payment" element={<Pdf />} />
         <Route path="/pro" element={<Pronav />} />
       </Routes>
       {isCartOpen && <CartSide toggleCart={toggleCart} />}
