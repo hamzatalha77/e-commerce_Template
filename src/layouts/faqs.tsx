43daff28d0bc233686e46feb7c3fbe19034b25a2
hamzatalha77 <hamza_ta7@hotmail.com>
2024-08-26 17:48:18 +0100

    commit

--- src/layouts/faqs.tsx
+++ src/layouts/faqs.tsx
@@ -7,7 +7,7 @@ const Faqs = () => {
       <h3 className="breadcrumb__subtitle">
         Home {'>'} <span className="text-red-600">FAQ'S</span>
       </h3>
-      <div className="questions__container grid grid-cols-[repeat(2,1fr)] gap-6">
+      <div className="questions__container grid grid-cols-[repeat(2,1fr)] gap-6 items-start">
         <div className="questions__group grid gap-y-8">
           <div className="questions_item rounded bg-white [transition:.3s]">
             <header className="questions__header flex items-center gap-x-2 p-4 cursor-pointer [transition:.3s]">
@@ -24,30 +24,30 @@ const Faqs = () => {
               </p>
             </div>
           </div>
-          <div className="questions_item">
-            <header className="questions__header">
-              <i className="bx bx-plus"></i>
-              <h3 className="questions__item-title">
+          <div className="questions_item rounded bg-white [transition:.3s]">
+            <header className="questions__header flex items-center gap-x-2 p-4 cursor-pointer [transition:.3s]">
+              <i className="questions_icon bx bx-plus text-xl text-[#242329] [transition:.3s]"></i>
+              <h3 className="questions__item-title text-sm font-medium [transition:.3s]">
                 Lorem ipsum dolor sit amet consectetur.
               </h3>
             </header>
-            <div className="questions_content">
-              <p className="questions_description">
+            <div className="questions_content overflow-hidden h-0 [transition:.3s]">
+              <p className="questions_description text-xs pt-[0] pr-14 pb-9 pl-11 [transition:.3s]">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
                 ratione natus ducimus reiciendis possimus alias dignissimos et
                 dolore quaerat? Sunt, est!
               </p>
             </div>
           </div>
-          <div className="questions_item">
-            <header className="questions__header">
-              <i className="bx bx-plus"></i>
-              <h3 className="questions__item-title">
+          <div className="questions_item rounded bg-white [transition:.3s]">
+            <header className="questions__header flex items-center gap-x-2 p-4 cursor-pointer [transition:.3s]">
+              <i className="questions_icon bx bx-plus text-xl text-[#242329] [transition:.3s]"></i>
+              <h3 className="questions__item-title text-sm font-medium [transition:.3s]">
                 Lorem ipsum dolor sit amet consectetur.
               </h3>
             </header>
-            <div className="questions_content">
-              <p className="questions_description">
+            <div className="questions_content overflow-hidden h-0 [transition:.3s]">
+              <p className="questions_description text-xs pt-[0] pr-14 pb-9 pl-11 [transition:.3s]">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
                 ratione natus ducimus reiciendis possimus alias dignissimos et
                 dolore quaerat? Sunt, est!
@@ -55,46 +55,46 @@ const Faqs = () => {
             </div>
           </div>
         </div>
-        <div className="questions__group">
-          <div className="questions_item">
-            <header className="questions__header">
-              <i className="bx bx-plus"></i>
-              <h3 className="questions__item-title">
+        <div className="questions__group grid gap-y-8">
+          <div className="questions_item rounded bg-white [transition:.3s]">
+            <header className="questions__header flex items-center gap-x-2 p-4 cursor-pointer [transition:.3s]">
+              <i className="questions_icon bx bx-plus text-xl text-[#242329] [transition:.3s]"></i>
+              <h3 className="questions__item-title text-sm font-medium [transition:.3s]">
                 Lorem ipsum dolor sit amet consectetur.
               </h3>
             </header>
-            <div className="questions_content">
-              <p className="questions_description">
+            <div className="questions_content overflow-hidden h-0 [transition:.3s]">
+              <p className="questions_description text-xs pt-[0] pr-14 pb-9 pl-11 [transition:.3s]">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
                 ratione natus ducimus reiciendis possimus alias dignissimos et
                 dolore quaerat? Sunt, est!
               </p>
             </div>
           </div>
-          <div className="questions_item">
-            <header className="questions__header">
-              <i className="bx bx-plus"></i>
-              <h3 className="questions__item-title">
+          <div className="questions_item rounded bg-white [transition:.3s]">
+            <header className="questions__header flex items-center gap-x-2 p-4 cursor-pointer [transition:.3s]">
+              <i className="questions_icon bx bx-plus text-xl text-[#242329] [transition:.3s]"></i>
+              <h3 className="questions__item-title text-sm font-medium [transition:.3s]">
                 Lorem ipsum dolor sit amet consectetur.
               </h3>
             </header>
-            <div className="questions_content">
-              <p className="questions_description">
+            <div className="questions_content overflow-hidden h-0 [transition:.3s]">
+              <p className="questions_description text-xs pt-[0] pr-14 pb-9 pl-11 [transition:.3s]">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
                 ratione natus ducimus reiciendis possimus alias dignissimos et
                 dolore quaerat? Sunt, est!
               </p>
             </div>
           </div>
-          <div className="questions_item">
-            <header className="questions__header">
-              <i className="bx bx-plus"></i>
-              <h3 className="questions__item-title">
+          <div className="questions_item rounded bg-white [transition:.3s]">
+            <header className="questions__header flex items-center gap-x-2 p-4 cursor-pointer [transition:.3s]">
+              <i className="questions_icon bx bx-plus text-xl text-[#242329] [transition:.3s]"></i>
+              <h3 className="questions__item-title text-sm font-medium [transition:.3s]">
                 Lorem ipsum dolor sit amet consectetur.
               </h3>
             </header>
-            <div className="questions_content">
-              <p className="questions_description">
+            <div className="questions_content overflow-hidden h-0 [transition:.3s]">
+              <p className="questions_description text-xs pt-[0] pr-14 pb-9 pl-11 [transition:.3s]">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
                 ratione natus ducimus reiciendis possimus alias dignissimos et
                 dolore quaerat? Sunt, est!
